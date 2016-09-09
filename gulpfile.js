4af8bbe1b64ecd497a8149fd52e7c31bd44b9dab
SkyRoxas <SkyRoxas1521@gmail.com>
2016-09-09 20:08:29 +0800

    browserSync basic

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,38 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     autoprefixer = require('gulp-autoprefixer');
+    browserSync = require('browser-sync').create();
 
 
 //gulp sass
-var sass_Config ={
-
-}
+var sass_Config ={}
 //autoprefixer config
 var autoprefixer_Config = {
     browsers: ['last 2 versions'],
     cascade: false
 }
+//browserSync config
+gulp.task('browser-sync',function(){
+  browserSync.init({
+    server:{
+            baseDir: "./"
+        }
+
+    //proxy: "你的域名或IP";
+  });
+  gulp.watch("**/*.html").on('change', reload);
+});
+
+
 gulp.task('sass', function() {
     gulp.src('sass/style.scss')
         .pipe(sourcemaps.init())
         .pipe(autoprefixer(autoprefixer_Config))
         .pipe(sass(sass_Config))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({stream:true}));
+
 })
 
 gulp.task('sass:watch', function() {
