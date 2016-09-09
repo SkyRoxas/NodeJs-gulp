37291f4c29f88cbb0ef81228ad268bca6d792313
SkyRoxas <SkyRoxas1521@gmail.com>
2016-09-09 18:27:36 +0800

    autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 //require module
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer');
 
 
 //gulp sass
+var sass_Config ={
+
+}
+//autoprefixer config
+var autoprefixer_Config = {
+    browsers: ['last 2 versions'],
+    cascade: false
+}
 gulp.task('sass', function() {
     gulp.src('sass/style.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass())
+        .pipe(autoprefixer(autoprefixer_Config))
+        .pipe(sass(sass_Config))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('css'));
 })
